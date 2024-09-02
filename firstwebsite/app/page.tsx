2c812bfbf0e5b88b57ab6e921376fcaedfa4103f
sim-daas <aitechmanml@gmail.com>
2024-09-02 05:32:01 +0000

    new

--- firstwebsite/app/page.tsx
+++ firstwebsite/app/page.tsx
@@ -3,6 +3,7 @@ export default function Page() {
     <div>
       <h1>Welcome to My Vercel Website</h1>
       <p>This is a basic demo website built with Next.js and deployed on Vercel.</p>
+      <p>Just a random edit</p>
     </div>
   );
 }
